387bcec1f6dc7d9c64069f3fcc16f7c95038a4ec
Kana <102465188+ElusiveFluffy@users.noreply.github.com>
2025-06-17 14:45:50 +0930

    Add some operator overloads to Vector4f

--- TygerMemory/vector4f.h
+++ TygerMemory/vector4f.h
@@ -2,5 +2,36 @@
 struct Vector4f
 {
 	float x, y, z, w;
+
+	Vector4f operator*(Vector4f const rhs) const {
+		return { x * rhs.x,
+				 y * rhs.y,
+				 z * rhs.z,
+				 w };
+	};
+	Vector4f operator+(Vector4f const rhs) const {
+		return { x + rhs.x,
+				 y + rhs.y,
+				 z + rhs.z,
+				 w };
+	};
+	Vector4f operator-(Vector4f const rhs) const {
+		return { x - rhs.x,
+				 y - rhs.y,
+				 z - rhs.z,
+				 w };
+	};
+	Vector4f operator/(Vector4f const rhs) const {
+		return { x / rhs.x,
+				 y / rhs.y,
+				 z / rhs.z,
+				 w };
+	};
+	Vector4f operator*(float const rhs) const {
+		return { x * rhs,
+				 y * rhs,
+				 z * rhs,
+				 w };
+	};
 };
 
