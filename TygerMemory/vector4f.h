7905c60763afb671a4af7d0d2a6be8d83a910b55
Kana <102465188+ElusiveFluffy@users.noreply.github.com>
2025-06-18 14:17:45 +0930

    Add normalize and cross functions to vector4f

--- TygerMemory/vector4f.h
+++ TygerMemory/vector4f.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <cmath>
+
 struct Vector4f
 {
 	float x, y, z, w;
@@ -33,5 +35,21 @@ struct Vector4f
 				 z * rhs,
 				 w };
 	};
+
+	static inline Vector4f Normalize(const Vector4f& vector) {
+		float length = std::sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
+		if (length == 0.0f) // Avoid division by zero
+			return { 0.0f, 0.0f, 0.0f, 1.0f };
+		return { vector.x / length, vector.y / length, vector.z / length, vector.w };
+	}
+
+	static inline Vector4f Cross(const Vector4f& a, const Vector4f& b) {
+		return {
+			a.y * b.z - a.z * b.y,
+			a.z * b.x - a.x * b.z,
+			a.x * b.y - a.y * b.x,
+			a.w
+		};
+	}
 };
 
